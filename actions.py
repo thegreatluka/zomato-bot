d49517e93854c30294e3ecffa17bf32384f1a2af
thegreatluka <sourav.sunil2004@gmail.com>
2020-10-19 17:57:49 +0530

    implemented validate location and cuisine

--- actions.py
+++ actions.py
@@ -3,106 +3,109 @@ from __future__ import division
 from __future__ import unicode_literals
 
 from rasa_sdk import Action
-from rasa_sdk.events import SlotSet
-from rasa.constants import DEFAULT_DATA_PATH
+from rasa_sdk.events import SlotSet, AllSlotsReset
 import zomatopy
 import json
+
+config = {"user_key": "acfcf315ed54c498179910378808b0c7"}
+DEFAULT_DATA_PATH = 'data'
 
 class ActionSearchRestaurants(Action):
 	def name(self):
 		return 'action_search_restaurants'
-		
+
 	def run(self, dispatcher, tracker, domain):
-		config={ "user_key":"acfcf315ed54c498179910378808b0c7"}
+
 		zomato = zomatopy.initialize_app(config)
 		loc = tracker.get_slot('location')
 		cuisine = tracker.get_slot('cuisine')
-		location_detail=zomato.get_location(loc, 1)
+		location_detail = zomato.get_location(loc, 1)
 		d1 = json.loads(location_detail)
-		lat=d1["location_suggestions"][0]["latitude"]
-		lon=d1["location_suggestions"][0]["longitude"]
-		cuisines_dict={'bakery':5,'chinese':25,'cafe':30,'italian':55,'biryani':7,'north indian':50,'south indian':85}
-		results=zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
+		lat = d1["location_suggestions"][0]["latitude"]
+		lon = d1["location_suggestions"][0]["longitude"]
+		cuisines_dict = {'bakery': 5, 'chinese': 25, 'cafe': 30,
+			'italian': 55, 'biryani': 7, 'north indian': 50, 'south indian': 85}
+		results = zomato.restaurant_search(
+			"", lat, lon, str(cuisines_dict.get(cuisine)), 5)
 		d = json.loads(results)
-		response=""
+		response = ""
 		if d['results_found'] == 0:
-			response= "no results"
+			response = "no results"
 		else:
 			for restaurant in d['restaurants']:
-				response=response+ "Found "+ restaurant['restaurant']['name']+ " in "+ restaurant['restaurant']['location']['address']+"\n"
-		
-		dispatcher.utter_message("-----"+response)
-		return [SlotSet('location',loc)]
+				response = response + "Found " + \
+					restaurant['restaurant']['name'] + " in " + \
+						restaurant['restaurant']['location']['address']+"\n"
 
+		dispatcher.utter_message("-----"+response)
+		return [SlotSet('location', loc)]
 
 
 class ActionValidateLocation(Action):
 
-    def name(self):
-        return "action_validate_location"
+	def name(self):
+		return "action_validate_location"
 
-    def run(self, dispatcher, tracker, domain):
-
-		config=	{ "user_key":"acfcf315ed54c498179910378808b0c7" }
-		zomato = zomatopy.initialize_app(config)
+	def run(self, dispatcher, tracker, domain):
 		location_validity = "valid"
+		location = tracker.get_slot("location")
 
 		if not location:
-            location_validity = "invalid"
-        else:
-            filepath = DEFAULT_DATA_PATH + "/cities.json"
+			location_validity = "invalid"
+		else:
+			filepath = DEFAULT_DATA_PATH + "/validCity.json"
 
-            with open(filepath) as cities_file:
+			with open(filepath) as cities_file:
 
-                data = json.load(cities_file)
+				data = json.load(cities_file)
 
-                if data is not None:
-                    tier1_cities = data["data"]["tier1"]
-                    tier2_cities = data["data"]["tier2"]
+				if data is not None:
+					tier1_cities = data["data"]["tier1"]
+					tier2_cities = data["data"]["tier2"]
 
-                    tier1_cities_lower = [city.lower() for city in tier1_cities]
-                    tier2_cities_lower = [city.lower() for city in tier2_cities]
+					tier1_cities_lower = [city.lower() for city in tier1_cities]
+					tier2_cities_lower = [city.lower() for city in tier2_cities]
 
-                    location_validity = (
-                        "invalid"
-                        if location.lower() not in tier1_cities_lower
-                        and location.lower() not in tier2_cities_lower
-                        else "valid"
-                    )
-                else:
-                    location_validity = "invalid"
+					location_validity = (
+						"invalid"
+						if location.lower() not in tier1_cities_lower
+						and location.lower() not in tier2_cities_lower
+						else "valid"
+					)
+				else:
+					location_validity = "invalid"
 
-        return [SlotSet("location_validity", location_validity)]
+		return [SlotSet("location_validity", location_validity)]
 
 
 
 class ActionValidateCuisine(Action):
-    def name(self):
-        return "action_validate_cuisine"
+	def name(self):
+		return "action_validate_cuisine"
 
-    def run(self, dispatcher, tracker, domain):
+	def run(self, dispatcher, tracker, domain):
 
-        cuisine = tracker.get_slot("cuisine")
-        cuisine_validity = "valid"
+		cuisine = tracker.get_slot("cuisine")
+		cuisine_validity = "valid"
 
-        if not cuisine:
-            cuisine_validity = "invalid"
-        else:
-            supported_cuisines = [
-                "american",
-                "chinese",
-                "italian",
-                "mexican",
-                "north indian",
-                "south indian",
-            ]
+		if not cuisine:
+			cuisine_validity = "invalid"
+		else:
+			supported_cuisines = [
+				"american",
+				"chinese",
+				"italian",
+				"mexican",
+				"north indian",
+				"south indian",
+			]
 
-            cuisine_validity = (
-                "invalid" if cuisine.lower() not in supported_cuisines else "valid"
-            )
+			cuisine_validity = (
+				"invalid" if cuisine.lower() not in supported_cuisines else "valid"
+			)
 
-        return [SlotSet("cuisine_validity", cuisine_validity)]
-        
+		return [SlotSet("cuisine_validity", cuisine_validity)]
+		
 
 		
 class ActionSlotReset(Action): 
